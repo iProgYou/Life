a9f62cf5ef41b5993581c0bfa70ce9a4d73d5e5d
iProgYou <zack.musicteacher@gmail.com>
2021-05-29 12:33:55 -0700

    add script

---
+++ README.md
@@ -0,0 +1,3 @@
+run `python3 script.py <insert grid size here>`
+
+ex. `python3 script.py 50`
---
+++ script.py
@@ -0,0 +1,6 @@
+import sys
+import os
+
+os.system(f"python3 grid.py {sys.argv[1]}")
+os.system("npm install")
+os.system("google-chrome index.html")
